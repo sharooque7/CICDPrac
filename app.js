38b67121745e2614a6aa2d22e5d0f01ba450a936
Sharooque7 <sharooquethoufiq@gmail.com>
2022-03-21 06:32:45 +0530

    AB#1

--- app.js
+++ app.js
@@ -13,4 +13,8 @@ app.get("/", (req, res, next) => {
   res.json({ message: "Git CI/CD Checkoout" });
 });
 
+app.get("/", (req, res, next) => {
+  res.json({ message: "Git CI/CD Azure Devops" });
+});
+
 app.listen(3000);
